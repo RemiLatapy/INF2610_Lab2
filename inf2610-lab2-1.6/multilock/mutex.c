2a24713a64d746fadaf53099acf5301b856d4707
Rémi Latapy <remi.latapy@insa-lyon.fr>
2015-09-30 21:16:55 -0400

    implemen mutex

--- inf2610-lab2-1.6/multilock/mutex.c
+++ inf2610-lab2-1.6/multilock/mutex.c
@@ -20,11 +20,13 @@ void *mutex_worker(void *ptr) {
 
     for (i = 0; i < exp->outer; i++) {
         // TODO: verrouiller
+		pthread_mutex_lock(&exp->lock);
         for (j = 0; j < exp->inner; j++) {
             unsigned long idx = (i * exp->inner) + j;
             statistics_add_sample(exp->data, (double) idx);
         }
         // TODO deverrouiller
+		pthread_mutex_unlock(&exp->lock);
     }
     return NULL;
 }
@@ -32,14 +34,18 @@ void *mutex_worker(void *ptr) {
 void mutex_init(struct experiment *exp) {
     exp->data = make_statistics();
     // TODO: allocation d'un pthread_mutex_t dans exp->lock
+	exp->lock = calloc(sizeof(pthread_mutex_t));
     // TODO: initialisation du mutex
+	pthread_mutex_init(&exp->lock, NULL);
 }
 
 void mutex_done(struct experiment *exp) {
     statistics_copy(exp->stats, exp->data);
     free(exp->data);
     // TODO: destruction du verrou
+	pthread_mutex_destroy(&exp->lock);
     // TODO: liberation de la memoire du verrou
+	free(exp->lock);
 }
 
 
