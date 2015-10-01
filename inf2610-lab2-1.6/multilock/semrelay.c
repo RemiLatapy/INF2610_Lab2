bdc63092561845cf7fe40f2a73dad9cfd7811de5
Rémi Latapy <remi.latapy@insa-lyon.fr>
2015-09-30 21:47:24 -0400

    implement semaphore

--- inf2610-lab2-1.6/multilock/semrelay.c
+++ inf2610-lab2-1.6/multilock/semrelay.c
@@ -21,11 +21,13 @@ void *semrelay_worker(void *ptr) {
 
     for (i = 0; i < exp->outer; i++) {
         // TODO: attendre notre tour
+		sem_wait(&exp->lock[i]);
         for (j = 0; j < exp->inner; j++) {
             unsigned long idx = (i * exp->inner) + j;
             statistics_add_sample(exp->data, (double) idx);
         }
         // TODO: signaler le travailleur suivant
+		sem_post(&exp->lock[i]);
     }
     return NULL;
 }
@@ -35,7 +37,11 @@ void semrelay_init(struct experiment *exp) {
 
     exp->data = make_statistics();
     // TODO: allocation d'un tableau de sémaphores sem_t dans exp->lock
+	exp->lock = *sem_t[i];
     // TODO: initialisation des sémaphores
+	for(int j = 0 ; j<i ; j++) {
+		sem_init(&exp->lock[j], 0, 1);
+	}
 }
 
 void semrelay_done(struct experiment *exp) {
@@ -46,6 +52,11 @@ void semrelay_done(struct experiment *exp) {
     free(exp->data);
 
     // TODO: destruction du verrou
+	for(int j = 0 ; j<i ; j++) {
+		sem_destroy(&exp->lock[j]);
+	}
+	
     // TODO: liberation de la memoire du verrou
+	free(exp->lock);
 }
 
