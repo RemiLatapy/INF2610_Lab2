7d7668efca5f662cda0c9b7373dd3fbf48f11b6b
RemiLatapy <remi.latapy@insa-lyon.fr>
2015-10-01 12:25:33 -0400

    spinlock ok

--- inf2610-lab2-1.6/multilock/spinlock.c
+++ inf2610-lab2-1.6/multilock/spinlock.c
@@ -7,6 +7,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 #include "minispinlock.h"
 #include "statistics.h"
 #include "multilock.h"
@@ -21,11 +22,13 @@ void *spinlock_worker(void *ptr) {
     struct experiment *exp = ptr;
     for (i = 0; i < exp->outer; i++) {
         // TODO: verrouiller
+        pthread_spin_lock(exp->lock);
         for (j = 0; j < exp->inner; j++) {
             unsigned long idx = (i * exp->inner) + j;
             statistics_add_sample(exp->data, (double) idx);
         }
         // TODO: deverrouiller
+        pthread_spin_unlock(exp->lock);
     }
     return NULL;
 }
@@ -33,12 +36,15 @@ void *spinlock_worker(void *ptr) {
 void spinlock_init(struct experiment *exp) {
     exp->data = make_statistics();
     // TODO: allocation d'un long dans exp->lock
+    exp->lock = calloc(1, sizeof(long));
     // TODO: initialisation à zéro
+     pthread_spin_init(exp->lock, 0);
 }
 
 void spinlock_done(struct experiment *exp) {
     statistics_copy(exp->stats, exp->data);
     free(exp->data);
     // TODO: liberation de la memoire du verrou
+    free(exp->lock);
 }
 
